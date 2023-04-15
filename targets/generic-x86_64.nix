8fc6f4abfc6cf10edc6279ef805ed87157293d66
Mika Tammi <mika.tammi@unikie.com>
2023-04-15 20:08:34 +0300

    AppVM with Firefox
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- targets/generic-x86_64.nix
+++ targets/generic-x86_64.nix
@@ -18,7 +18,7 @@
       modules =
         [
           (import ../modules/host {
-            inherit self microvm netvm;
+            inherit self microvm netvm firefoxvm;
           })
 
           ../modules/hardware/x86_64-linux.nix
@@ -48,8 +48,9 @@
         ++ extraModules;
     };
     netvm = "netvm-${name}-${variant}";
+    firefoxvm = "appvm-firefox-${name}-${variant}";
   in {
-    inherit hostConfiguration netvm;
+    inherit hostConfiguration netvm firefoxvm;
     name = "${name}-${variant}";
     netvmConfiguration =
       (import ../microvmConfigurations/netvm {
@@ -76,6 +77,9 @@
           }
         ];
       };
+    firefoxvmConfiguration = import ../microvmConfigurations/appvm-firefox {
+      inherit nixpkgs microvm system;
+    };
     package = hostConfiguration.config.system.build.${hostConfiguration.config.formatAttr};
   };
   debugModules = [../modules/development/usb-serial.nix];
@@ -86,7 +90,8 @@
 in {
   nixosConfigurations =
     builtins.listToAttrs (map (t: nixpkgs.lib.nameValuePair t.name t.hostConfiguration) targets)
-    // builtins.listToAttrs (map (t: nixpkgs.lib.nameValuePair t.netvm t.netvmConfiguration) targets);
+    // builtins.listToAttrs (map (t: nixpkgs.lib.nameValuePair t.netvm t.netvmConfiguration) targets)
+    // builtins.listToAttrs (map (t: nixpkgs.lib.nameValuePair t.firefoxvm t.firefoxvmConfiguration) targets);
   packages = {
     x86_64-linux =
       builtins.listToAttrs (map (t: nixpkgs.lib.nameValuePair t.name t.package) targets);
