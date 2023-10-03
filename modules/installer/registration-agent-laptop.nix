acfb68aee54da84c2923bb0abb0bd8112ba7ea03
Anh Huy Bui <buianhhuy96@gmail.com>
2023-10-03 11:58:49 +0300

    Installer-RegistrationAgent: Update to version v0.3.0

--- modules/installer/registration-agent-laptop.nix
+++ modules/installer/registration-agent-laptop.nix
@@ -15,8 +15,10 @@ in
 buildGo121Module {
   name = "registration-agent-laptop";
   src = builtins.fetchGit {
-    url = "https://github.com/tiiuae/registration-agent-laptop/";
-    rev = "fec3dfce76707c8f8541b69996ea1f09d474c16d";
+    url = "git@github.com:tiiuae/registration-agent-laptop.git";
+    # Testing with tags name but failed at pure evaluation
+    #ref = "refs/tags/v0.3.0";
+    rev = "bb3f0180a544700cfeeb2e3cb97c5b1996401ca8";
   };
   vendorSha256 = "sha256-azmIkOfmHLL+xM9mJabsUT0EJTKoI97Sq1lKARG5cU8=";
   proxyVendor=true;
